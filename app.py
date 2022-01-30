879ba9d2a50129a7426f826377d010f6ff500dde
Dmitrii Gornostaev <d.gornostaev@zedplus.ru>
2022-01-30 17:25:30 +0300

    DATABASE_URL for Postgres and Heroku

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 
 
 import os
+import re
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -15,7 +16,12 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+else:
+    uri = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
