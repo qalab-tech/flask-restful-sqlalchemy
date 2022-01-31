ea2aa9954d4bdd1d2115cc2032a83e2069f822da
Dmitrii Gornostaev <d.gornostaev@zedplus.ru>
2022-01-31 22:28:56 +0300

    Heroku SQLite DB application

--- app.py
+++ app.py
@@ -15,7 +15,13 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+else:
+    uri = 'sqlite:///data.db'
+# rest of connection code using the connection string `uri`
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
